9c29a4b29efe7507b4c574c69e1a78dbfaed16f6
Alexandr Samorokowsky <samorokowsky@hotmail.com>
2013-12-08 12:52:00 +0400

    Создан пустой проект

---
+++ VM_3_5/VM_3_5/FrmMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VM_3_5
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
